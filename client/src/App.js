e4554aec305ee4f883951732b02078267abe33b6
arbiva <arbiva@post.bgu.ac.il>
2021-12-18 11:42:38 +0200

    prior to styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,32 @@
 import React from "react";
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import './App.css';
+import seaparets from "./images/seaparets.png";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Forms/Form";
 
 function App() {
   return (
-    <div className="App">
-        <h1>movie project</h1>
-    </div>
+    <Container maxWidth="lg">
+<AppBar position="static" color="inherit">
+  <Typography  variant="h2" align="center">
+movie critique
+  </Typography>
+  <img src={seaparets} alt="img" height="500" />
+</AppBar>
+<Grow in>
+  <Container>
+    <Grid container justify="space between" alighItems="stretch" spacing={3}>
+<Grid item xs={12} sm={7}>
+  <Posts />
+</Grid>
+<Grid item xs={12} sm={4}>
+  <Form />
+</Grid>
+    </Grid>
+  </Container>
+</Grow>
+    </Container>
   );
 }
 
